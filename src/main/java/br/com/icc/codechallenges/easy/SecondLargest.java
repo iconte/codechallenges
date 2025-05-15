d7fdf32e2a557e9ea5cd3fd16cc30a15d54824d4
Iva Conte <ivconte@gmail.com>
2025-05-14 21:05:46 -0300

    add secondLargest

---
+++ src/main/java/br/com/icc/codechallenges/easy/SecondLargest.java
@@ -0,0 +1,35 @@
+package br.com.icc.codechallenges.easy;
+
+import java.util.List;
+
+public class SecondLargest {
+
+	public static void main(String[] args) {
+		int[] array = new int[] { 1, 40, 50, 25, 25 };
+		System.out.println(getSecondLargest(array));
+	}
+
+	public static int getSecondLargest(int[] arr) {
+		// code here
+		int first = -1;
+		int second = -1;
+		int tam = arr.length;
+		if (tam < 2) {
+			return second;
+		}
+
+		for (int i = 0; i < tam; i++) {
+			if (arr[i] > first) {
+				second = first;
+				first = arr[i];
+			}
+			if (arr[i] > second && arr[i] != first) {
+				second = arr[i];
+			}
+		}
+
+		return second;
+
+	}
+
+}
